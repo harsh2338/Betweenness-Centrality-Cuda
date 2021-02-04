c66639064c273e6b559d8dbaa8c11bc6d8ca540f
harsh2338 <harsh.aayush.r1@gmail.com>
2021-02-04 15:18:19 +0530

    Added final report

--- test.cpp
+++
@@ -1,9 +0,0 @@
-#include<bits/stdc++.h>
-#include "graph.h"
-using namespace std;
-int main(){
-	freopen("input.txt", "r", stdin);
-    Graph *graph = new Graph();
-    graph->readGraph();
-    graph->convertToCOO();
-}
